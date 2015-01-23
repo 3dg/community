81e14c5113dccc07a0c4d3cae622075cc87a19e7
rhyzx <rhyzix@gmail.com>
2015-01-23 23:05:19 +0800

    default off email mention

--- config.extra.php
+++ config.extra.php
@@ -1,5 +1,7 @@
 <?php
 $config = array();
+
+// openshift env
 if ( getenv('OPENSHIFT_APP_NAME') ) {
   $config["esoTalk.database.host"] = getenv('OPENSHIFT_MYSQL_DB_HOST');
   $config["esoTalk.database.port"] = getenv('OPENSHIFT_MYSQL_DB_PORT');
@@ -8,3 +10,8 @@ if ( getenv('OPENSHIFT_APP_NAME') ) {
   $config["esoTalk.database.dbName"] = getenv('OPENSHIFT_APP_NAME');
   // $config["esoTalk.baseURL"] = 'http://3dgundam.com/';
 }
+
+// default off mention email
+$config["esoTalk.preferences.email.privateAdd"] = false;
+$config["esoTalk.preferences.email.post"] = false;
+$config["esoTalk.preferences.email.mention"] = false;
