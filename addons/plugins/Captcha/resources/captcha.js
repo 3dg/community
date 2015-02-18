52b3891f68bf7272ebb01573b8d165addb6322b9
rhyzx <rhyzix@gmail.com>
2015-02-18 21:47:50 +0800

    update

--- addons/plugins/Captcha/resources/captcha.js
+++ addons/plugins/Captcha/resources/captcha.js
@@ -4,13 +4,14 @@ void function () {
 $(document).off('click.captcha')
 .on('click.captcha', '.captcha img:not(.loading)', function () {
   var t = $.now()
-  var $img = $('<img src="/captcha?t=' + t + '" srcset="/captcha/2x?t=' + t + ' 2x">')
-  $(this).replaceWith($img)
 
-  $img.addClass('loading')
+  var $img = $(this)
+  .addClass('loading')
   .one('load error', function () {
     $img.removeClass('loading')
   })
+  .attr('src', '/captcha?t=' + t)
+  .attr('srcset', '/captcha/2x?t=' + t + ' 2x')
 })
 
 // tooltip
