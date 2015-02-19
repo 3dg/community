116ad4453d79008fd49f3d2127384f50a8fa8341
rhyzx <rhyzix@gmail.com>
2015-02-19 15:11:20 +0800

    update protocol

--- addons/plugins/Upyun/resources/upyun.js
+++ addons/plugins/Upyun/resources/upyun.js
@@ -101,8 +101,10 @@ function upload(file, $target) {
   ETConversation.wrapText($target, placeholder, '', '', '')
   deferred
   .done(function (res, bucket) {
-    result = '[url=//' + bucket + '.b0.upaiyun.com' + res.url + ']'
-      + '[img]//' + bucket + '.b0.upaiyun.com' + res.url + '!s[/img]'
+    result = '[url=http://' + bucket + '.b0.upaiyun.com' + res.url + ']'
+      // TODO thumb name setting
+      // TODO https?
+      + '[img]http://' + bucket + '.b0.upaiyun.com' + res.url + '!s[/img]'
       + '[i]' + res['image-width'] + 'x' + res['image-height'] + '[/i]'
       + '[/url]'
   })
